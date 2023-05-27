364e796431cff934cbe49e122445c5afbf421f3d
R Venkat <130206838+venkat-547@users.noreply.github.com>
2023-05-27 14:14:37 +0530

    Add files via upload

---
+++ task3/main.js
@@ -0,0 +1,22 @@
+function convertToFahrenheit() {
+    const celsiusInput = document.getElementById('celsius');
+    const fahrenheitInput = document.getElementById('fahrenheit');
+  
+    if (celsiusInput.value !== '') {
+      const celsius = parseFloat(celsiusInput.value);
+      const fahrenheit = (celsius * 9/5) + 32;
+      fahrenheitInput.value = fahrenheit.toFixed(2);
+    }
+  }
+  
+  function convertToCelsius() {
+    const fahrenheitInput = document.getElementById('fahrenheit');
+    const celsiusInput = document.getElementById('celsius');
+  
+    if (fahrenheitInput.value !== '') {
+      const fahrenheit = parseFloat(fahrenheitInput.value);
+      const celsius = (fahrenheit - 32) * 5/9;
+      celsiusInput.value = celsius.toFixed(2);
+    }
+  }
+  
